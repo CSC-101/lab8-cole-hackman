b3819f4283796b84e3cbc0dee99102a5b328a94d
cole-hackman <hackman@calpoly.edu>
2024-11-13 13:17:45 -0800

    Fixed the bug in groups_of_3 to ensure that if there are not enough values to fill the last grouping, the sublist will still contain the remaining values. Also developed 2 test functions to check for the correct output from groups_of_3

--- .github/group.py
+++ .github/group.py
@@ -2,9 +2,8 @@ def groups_of_3(input_list: list[int]) -> list[list[int]]:
     output_list = []
     for i in range(0, len(input_list), 3):
         temp = []
-        if i+2 <= input_list[i+2]:
-            for j in range(i,i+3):
-                temp.append(input_list[j])
+        for j in range(i,min(i+3, len(input_list))):
+            temp.append(input_list[j])
         output_list.append(temp)
     return output_list
 
--- .github/group_tests.py
+++ .github/group_tests.py
@@ -1,8 +1,8 @@
 import group
-import groups_of_3
 
 def main():
 
-    test_groups_of_3_1 = groups_of_3([1,2,3,4,5,6,7,8,9])
+    test_groups_of_3_1 = print(group.groups_of_3([1,2,3,4,5,6,7,8,9]))
+    test_groups_of_3_2 = print(group.groups_of_3([1, 2, 3, 4, 5, 6, 7, 8]))
 
 main()
